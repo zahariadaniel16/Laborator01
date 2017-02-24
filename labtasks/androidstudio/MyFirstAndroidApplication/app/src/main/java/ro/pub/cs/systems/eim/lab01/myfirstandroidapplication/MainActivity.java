ffcb58be8dcceb15dec6d3dd4c06782878b7308b
zahariadaniel16 <zaharia.daniel16@gmail.com>
2017-02-24 15:24:26 +0200

    implemented functionality for customized message and fade effect

--- labtasks/androidstudio/MyFirstAndroidApplication/app/src/main/java/ro/pub/cs/systems/eim/lab01/myfirstandroidapplication/MainActivity.java
+++ labtasks/androidstudio/MyFirstAndroidApplication/app/src/main/java/ro/pub/cs/systems/eim/lab01/myfirstandroidapplication/MainActivity.java
@@ -25,8 +25,12 @@ public class MainActivity extends AppCompatActivity {
             greetingTextView.setAlpha(1);
 
             // TODO: exercise 9a
-
+            greetingTextView.setText(greetingTextView.getText().toString().replace("xxx", "\n"+userNameEditText.getText()));
             // TODO: exercise 9b
+            AlphaAnimation fadeEffect = new AlphaAnimation(1.0f, 0.0f);
+            fadeEffect.setDuration(TRANSPARENCY_EFFECT_DURATION);
+            fadeEffect.setFillAfter(true);
+            greetingTextView.setAnimation(fadeEffect);
         }
 
     }
